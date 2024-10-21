d5d9a81cd72b0c18b01b9cadad9863f1341c8f1e
daoyuli <daoyuli123@gmail.com>
2024-10-22 00:20:08 +0100

    this is a comment for the git comment commit

---
+++ Ps1 -2 copy.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  1 19:05:29 2024
+
+@author: lidaoyu
+"""
+
+# Assume s is a string of lower case characters.
+
+# Write a program that prints the number of times the string 'bob' occurs in s.
+# For example, if s = 'azcbobobegghakl', then your program should print
+# Number of times bob occurs is: 2
+
+s ='azcbobobegghakl'
+count = 0 
+for i in range (len(s)-2):
+    if s[i:i+3] == 'bob':
+        # print (s[i:i+3])
+        count +=1
+print (count)
+    
+   
+    
